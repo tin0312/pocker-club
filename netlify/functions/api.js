bb7ed11713a4b601ba7deda90037453de5b61f0a
tin0312 <105316547+tin0312@users.noreply.github.com>
2024-03-28 02:05:38 -0400

    Try using an absolute path to render confirmation page

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // });
 app.get("/confirmation.html", (req, res) => {
     if(isSubmitted){
-      res.sendFile("confirmation.html", { root: "./dist" });   
+      res.sendFile("confirmation.html", { root: "dist" });   
     } else {
       res.redirect("/");
     } 
