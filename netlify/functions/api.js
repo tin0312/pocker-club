90286f966ca4b42582ae809c1edae4498bc16550
unknown <justinhoang0312@gmail.com>
2024-03-31 23:35:27 -0400

    Remove html file extension when redirecting route in express

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 import nodemailer from "nodemailer";
 import serverless from "serverless-http";
 import path from "path";
-import fs from "fs";
 
 const app = express();
 const router = Router();
@@ -15,62 +14,59 @@ let isSubmitted = false;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Handle access to confirmation page
-app.get("/confirmation.html", (req, res) => {
-    try {
-        if (isSubmitted) {
-            // Serve confirmation.html if isSubmitted is true
-            const resolved = process.env.LAMBDA_TASK_ROOT
-            ? path.resolve(process.env.LAMBDA_TASK_ROOT, "../../../confirmation.html")
-            : path.resolve(__dirname, "../../../dist/confirmation.html");
-        
-            res.sendFile(resolved);
-        } else {
-            // Redirect to home page if isSubmitted is false
-            res.redirect("/");
-        }
-    } catch (error) {
-        console.error("Error reading confirmation.html:", error);
-        res.status(500).send("Failed to load confirmation page.");
+app.get("/confirmation", (req, res) => {
+  try {
+    if (isSubmitted) {
+      res.sendFile(
+        path.resolve(__dirname, "../../../../../dist/confirmation.html")
+      );
+    } else {
+      // Redirect to home page if isSubmitted is false
+      res.redirect("/");
     }
+  } catch (error) {
+    console.error("Error reading confirmation.html:", error);
+    res.status(500).send("Failed to load confirmation page.");
+  }
 });
 
 // Handle submission route
 router.post("/form-submission", (req, res) => {
-    isSubmitted = true;
-    // Extract form data from the request body
-    const { fname, lname, email, phone, msg } = req.body;
-    // Send email using Nodemailer
-    sendEmail(fname, lname, email, phone, msg)
-        .then(() => {
-            console.log("Email sent successfully!");
-            res.redirect("/confirmation.html");
-            setTimeout(() => {
-                isSubmitted = false;
-            }, 3000);
-        })
-        .catch((error) => {
-            console.error("Error sending email:", error);
-            res.status(500).send("Failed to submit form. Please try again later.");
-        });
+  isSubmitted = true;
+  // Extract form data from the request body
+  const { fname, lname, email, phone, msg } = req.body;
+  // Send email using Nodemailer
+  sendEmail(fname, lname, email, phone, msg)
+    .then(() => {
+      console.log("Email sent successfully!");
+      res.redirect("/confirmation");
+      setTimeout(() => {
+        isSubmitted = false;
+      }, 3000);
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+      res.status(500).send("Failed to submit form. Please try again later.");
+    });
 });
 
 // Set up Email services
 async function sendEmail(fname, lname, email, phone, msg) {
-    // Create a Nodemailer transporter
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "justinhoang0312@gmail.com",
-            pass: "bftd rapg glnw zeyh",
-        },
-    });
+  // Create a Nodemailer transporter
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "justinhoang0312@gmail.com",
+      pass: "bftd rapg glnw zeyh",
+    },
+  });
 
-    // Email message options
-    let mailOptions = {
-        from: email,
-        to: "justinhoang0312@gmail.com",
-        subject: "Booking",
-        html: `
+  // Email message options
+  let mailOptions = {
+    from: email,
+    to: "justinhoang0312@gmail.com",
+    subject: "Booking",
+    html: `
             <div class="container">
                 <h2 class="title">New appointment</h2>
                 <p class="info"><strong>First Name:</strong> ${fname}</p>
@@ -82,10 +78,10 @@ async function sendEmail(fname, lname, email, phone, msg) {
                 </div>
             </div>
         `,
-    };
+  };
 
-    // Send the email
-    await transporter.sendMail(mailOptions);
+  // Send the email
+  await transporter.sendMail(mailOptions);
 }
 
 // Use the router middleware for routes under /api/
