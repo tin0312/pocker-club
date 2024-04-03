963598a9fba6a647229399087fd5e895de5217b2
tin0312 <105316547+tin0312@users.noreply.github.com>
2024-04-02 20:51:19 -0400

    Add and modify input field for wailist and setup Twilio SMS message

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,11 +4,17 @@ import bodyParser from "body-parser";
 import nodemailer from "nodemailer";
 import serverless from "serverless-http";
 import "dotenv/config";
+// import mysql from 'mysql2/promise';
+import { Twilio } from "twilio";
 
 const app = express();
 const router = Router();
-
+let waitlist = [];
 let isSubmitted = false;
+
+const accountSid = "AC1f28e845495a8eb1df7c6e7b440cef40";
+const authToken = "f3716686e347e2c3bb4845ca0351b8ec";
+const twilioClient = new Twilio(accountSid, authToken);
 
 // Parse request body
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,9 +37,9 @@ app.get("/confirmation", (req, res) => {
 router.post("/form-submission", (req, res) => {
   isSubmitted = true;
   // Extract form data from the request body
-  const { fname, lname, email, phone, msg } = req.body;
-  // Send email using Nodemailer
-  sendEmail(fname, lname, email, phone, msg)
+  const { fname, lname, email, phone, partySize, game } = req.body;
+ 
+  sendEmail(fname, lname, email, phone, partySize, game)
     .then(() => {
       console.log("Email sent successfully!");
       res.redirect("/confirmation");
@@ -41,6 +47,13 @@ router.post("/form-submission", (req, res) => {
       setTimeout(() => {
         isSubmitted = false;
       }, 3000);
+      twilioClient.messages
+        .create({
+          body: "Text Message from Omega Poker Club",
+          from: "+12512929460",
+          to: phone,
+        })
+        .then((message) => console.log(message.sid));
     })
     .catch((error) => {
       console.error("Error sending email:", error);
@@ -49,7 +62,7 @@ router.post("/form-submission", (req, res) => {
 });
 
 // Set up Email services
-async function sendEmail(fname, lname, email, phone, msg) {
+async function sendEmail(fname, lname, email, phone, partySize, game) {
   // Create a Nodemailer transporter
   let transporter = nodemailer.createTransport({
     service: "gmail",
@@ -63,34 +76,38 @@ async function sendEmail(fname, lname, email, phone, msg) {
   let adminMailOptions = {
     from: email,
     to: process.env.EMAIL_APP,
-    subject: "Booking",
+    subject: "Omega Poker Club - Booking",
     html: `
-            <div class="container">
-                <h2 class="title">New appointment</h2>
-                <p class="info"><strong>First Name:</strong> ${fname}</p>
-                <p class="info"><strong>Last Name:</strong> ${lname}</p>
-                <p class="info"><strong>Email:</strong> ${email}</p>
-                <p class="info"><strong>Phone:</strong> ${phone}</p>
-                <div class="message">
-                    <p>${msg}</p>
-                </div>
-            </div>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; border-radius: 10px; background-color: #f9f9f9;">
+    <h2 style="color: #333; text-align: center; margin-bottom: 50px;">New Appointment</h2>
+    <div style="margin-bottom: 20px;">
+        <p style="margin: 20px 0;"><strong>First Name:</strong> ${fname}</p>
+        <p style="margin: 20px 0;"><strong>Last Name:</strong> ${lname}</p>
+        <p style="margin: 20px 0;"><strong>Email:</strong> ${email}</p>
+        <p style="margin: 20px 0;"><strong>Phone:</strong> ${phone}</p>
+        <p style="margin: 20px 0;"><strong>Game:</strong> ${game}</p>
+        <p style="margin: 20px 0;"><strong>Party Size:</strong> ${partySize}</p>
+    </div>
+</div>
         `,
   };
 
   let userMailOption = {
     from: process.env.EMAIL_APP,
     to: email,
-    subject: "Booking",
+    subject: "Omega Poker Club - Booking",
     html: `
-      <div class="container">
-        <h2 class="title">Booking Confirmation</h2>
-        <p class="info">Thank you for booking with us!</p>
-        <div class="message">
-          <p>We have received your booking request and will get back to you shortly.</p>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
+        <h2 style="color: #333; text-align: center; margin-bottom: 20px;">Booking Confirmation</h2>
+        <p style="color: #666; text-align: center; margin-bottom: 50px;">Thank you for booking with us!</p>
+        <div style="background-color: #f9f9f9; padding: 20px; border-radius: 10px; margin-bottom: 50px;">
+            <p style="color: #333; margin: 0;text-align: center;">We have received your booking request and will get
+                back to you shortly.
+            </p>
         </div>
-      </div>
-      <p style="text-align: center;">Toronto Omega Poker - TOP club</p>
+        <p style="color:#14343b; margin-bottom: 20px;font-weight: bold; font-style: italic;">Toronto Omega Poker - TOP
+            club</p>
+    </div>
     `,
   };
 
