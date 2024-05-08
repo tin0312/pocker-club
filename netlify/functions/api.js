7af9ab805e1398d0c3a6c0df2048c76e05278e9f
tin0312 <105316547+tin0312@users.noreply.github.com>
2024-05-07 20:37:15 -0400

    Put redirect first in post route handler and add default option for game selection

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -58,6 +58,7 @@ app.get("/confirmation", (req, res) => {
 // Handle submission route
 router.post("/form-submission", async (req, res) => {
   isSubmitted = true;
+  res.redirect("/confirmation.html");
   // Extract form data from the request body
   const { fname, lname, email, phone, msg, game } = req.body;
   // Send email using Nodemailer
@@ -73,7 +74,6 @@ router.post("/form-submission", async (req, res) => {
     ).catch((error) => {
       console.error("Error sending SMS:", error);
     });
-  res.redirect("/confirmation.html");
 });
 
 // Set up Email services
